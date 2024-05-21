0335852c19d0e3f990b072b7a3ea24956f9aa6f1
Dikkoo <dikkoo@ecut.edu.cn>
2024-05-21 19:24:41 +0800

    文件夹名称修改+吴恩达系列init

---
+++ AIGC-dikkoo/andrew_ng/README.md
@@ -0,0 +1,7 @@
+# 吴恩达系列
+
+## Prompt Engineering 课程
+
+### 认识Prompt
+
+LLM是金矿，则Prompt是钥匙。
---
+++ AIGC-dikkoo/andrew_ng/demo/main.js
@@ -0,0 +1,3 @@
+// node里面的关键字"require"是用来引入模块的，这里引入了openai-api模块
+const OpenAI = require('openai-api'); // Import the OpenAI API
+
