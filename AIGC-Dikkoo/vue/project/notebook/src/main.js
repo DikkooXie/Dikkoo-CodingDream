21d33c4eda952f98afe78f45274ea50a64cfc785
Dikkoo <1095667169@qq.com>
2024-07-31 10:07:18 +0800

    添加reset.css，用于样式重置

--- AIGC-Dikkoo/vue/project/notebook/src/main.js
+++ AIGC-Dikkoo/vue/project/notebook/src/main.js
@@ -5,6 +5,8 @@ import 'lib-flexible/flexible.js'
 import App from './App.vue'
 import router from './router'
 
+import './assets/css/reset.css'
+
 const app = createApp(App)
 
 app.use(createPinia())
