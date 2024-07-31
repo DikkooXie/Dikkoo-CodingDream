1490c7639ec3b4c335e78f8f1d323ab06e8a53c8
Dikkoo <1095667169@qq.com>
2024-07-31 10:19:03 +0800

    引入前端组件库vant4

--- AIGC-Dikkoo/vue/project/notebook/src/App.vue
+++ AIGC-Dikkoo/vue/project/notebook/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     项目初始化完成
+    <van-button type="primary">主要按钮</van-button>
   </div>
 </template>
 
--- AIGC-Dikkoo/vue/project/notebook/src/main.js
+++ AIGC-Dikkoo/vue/project/notebook/src/main.js
@@ -1,15 +1,18 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import 'lib-flexible/flexible.js'
+import { Button } from 'vant' // 按需引入vant组件
 
 import App from './App.vue'
 import router from './router'
 
 import './assets/css/reset.css'
+import 'vant/lib/index.css' // 全局引入vant样式
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(Button) // 使用vant组件
 
 app.mount('#app')
