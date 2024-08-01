2dc241367b922264e19d099a6f79716619a87f53
Dikkoo <1095667169@qq.com>
2024-08-01 15:56:20 +0800

    后端数据库连接配置

---
+++ AIGC-Dikkoo/vue/project/notebook/server/config/index.js
@@ -0,0 +1,11 @@
+const config = {
+    database: {
+        DATABASE: 'notebook',
+        USERNAME: 'notebook',
+        PASSWORD: '12345678',
+        PORT: '3306',
+        HOST: 'admin.dikkoo.top'
+    }
+}
+
+module.exports = config;
