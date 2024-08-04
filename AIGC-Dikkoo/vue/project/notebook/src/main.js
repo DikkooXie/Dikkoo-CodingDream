5c2deef7c47d4e45a62d21e501dc042e4fb30efa
Dikkoo <1095667169@qq.com>
2024-08-04 12:33:50 +0800

    配置vant自动按需引入

--- AIGC-Dikkoo/vue/project/notebook/src/main.js
+++ AIGC-Dikkoo/vue/project/notebook/src/main.js
@@ -1,21 +1,15 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import 'lib-flexible/flexible.js'
-import { Button, Form, Field, CellGroup } from 'vant' // 按需引入vant组件
 
 import App from './App.vue'
 import router from './router'
 
 import './assets/css/reset.css'
-import 'vant/lib/index.css' // 全局引入vant样式
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-app.use(Button) // 使用vant组件
-app.use(Form);
-app.use(Field);
-app.use(CellGroup);
 
 app.mount('#app')
--- AIGC-Dikkoo/vue/project/notebook/vite.config.js
+++ AIGC-Dikkoo/vue/project/notebook/vite.config.js
@@ -2,11 +2,20 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import { VantResolver } from '@vant/auto-import-resolver';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    AutoImport({
+      resolvers: [VantResolver()],
+    }),
+    Components({
+      resolvers: [VantResolver()],
+    }),
   ],
   resolve: {
     alias: {
