df5a1ea733f52b9ab9fa03e930d035ef24823ac0
Dikkoo <1095667169@qq.com>
2024-08-06 09:21:29 +0800

    axios添加请求拦截，为所有请求添加token

--- AIGC-Dikkoo/vue/project/notebook/src/api/index.js
+++ AIGC-Dikkoo/vue/project/notebook/src/api/index.js
@@ -6,6 +6,21 @@ axios.defaults.baseURL = 'http://localhost:3000';
 // 设置默认请求头
 axios.defaults.headers.post['Content-Type'] = 'application/json;charset=utf-8';
 
+// 请求拦截器
+axios.interceptors.request.use(req => { // 在发送请求之前做些什么
+    // 读取用户 token
+    const token = localStorage.getItem('token');
+
+    if(token) { // 如果有 token 则添加到请求头
+        req.headers.Authorization = token;
+    }
+    
+    return req; // 返回请求
+}, error => {
+    // 异常处理
+    return Promise.reject(error);
+});
+
 // 响应拦截器
 axios.interceptors.response.use(res => {
     if(res.status !== 200) {
