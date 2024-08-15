9fe20ba9c723071db988e4bfac8ecbeef64879a0
Dikkoo <1095667169@qq.com>
2024-08-15 16:41:42 +0800

    进度条组件

---
+++ AIGC-Dikkoo/react/project/ai-translate/src/components/Progress.jsx
@@ -0,0 +1,16 @@
+export default function Progress({ text, percentage }) {
+    percentage = percentage ?? 0; // ES6+ 语法： ?? 空值合并运算符，如果percentage为null或undefined，则使用0
+    // 本质上是三元运算符的简写，当percentage为null或undefined时，使用0，否则使用percentage
+
+    return (
+        <div className="progress-container">
+            <div    
+                className="progress-bar"
+                style={{'width': `${percentage}`}}
+            >
+            {text} {`${percentage.toFixed(2)}%`}
+            </div>
+        </div>
+    )
+
+}
